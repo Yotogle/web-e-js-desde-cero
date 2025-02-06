7ac66887af4c2d8c1beeb06ffff6a97e2a65ccae
Yotogle <jjtg630@gmail.com>
2025-02-06 00:53:41 -0600

    first commit

---
+++ 1-fundamentos-js/tipos-de-dato.js
@@ -0,0 +1,18 @@
+
+console.log( typeof 42)
+
+console.log( typeof "venticinco" )
+
+console.log( typeof -666) 
+
+console.log( typeof true)
+
+console.log( typeof 0)
+
+console.log( typeof '')
+
+console.log( typeof null)
+
+console.log( typeof undefined)
+
+console.log( typeof false)
